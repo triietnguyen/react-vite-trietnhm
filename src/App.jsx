cc9ce9fe11f06b1f7402afb9e44d93f06d6ff2bc
triietnguyen <trietnguyenhoangminh@gmail.com>
2024-09-08 14:31:00 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,21 @@ import reactLogo from './assets/react.svg'
 
 const App = ()=> {
 
+  const trietnhm = 'Chiet';
+  const age = 12;
+  const data = {
+    address: "hanoi",
+    country: "VietNam",
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew/>
-      <TodoData/>
+      <TodoData
+        name={trietnhm}
+        age={age}
+        data={data}/>
       <div className='todo-image'>
         <img src={reactLogo} className='logo'></img>
       </div>
--- src/components/todo/todoData.jsx
+++ src/components/todo/todoData.jsx
@@ -1,6 +1,9 @@
-const TodoData = () => {
+const TodoData = (props) => {
+
+    const {name, age, data} = props
     return (
         <div className='todo-data'>
+        <div>{name}</div>
         <div> Learning React</div>
         <div> Watching Youtube</div>
       </div>
