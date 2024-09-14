9a528908b1c3771cb3419a8c5efd03aca13f4793
triietnguyen <trietnguyenhoangminh@gmail.com>
2024-09-14 12:36:30 +0700

    video 49

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,13 +1,26 @@
-import './header.css';
+import { Link } from "react-router-dom";
+import "./header.css";
 
-const Header = ()=>{
-    return (
-        <ul>
-            <li><a className="active" href="/">Home</a></li>
-            <li><a href="/users">Users</a></li>
-            <li><a href="/products">Products</a></li>
-        </ul>
-    )
-}
+const Header = () => {
+  return (
+    <ul>
+      <li>
+        <Link a className="active" to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link a to="/users">
+          Users
+        </Link>
+      </li>
+      <li>
+        <Link a to="/products">
+          Products
+        </Link>
+      </li>
+    </ul>
+  );
+};
 
 export default Header;
